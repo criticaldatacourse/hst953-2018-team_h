ebbd37b4a06fbd93450842ce52d28ed940db9734
MIngyulu <q8888620002@users.noreply.github.com>
2018-11-16 11:25:01 -0500

    Update pt_included_received_ns.sql

--- analysis/sql/pt_included_received_ns.sql
+++ analysis/sql/pt_included_received_ns.sql
@@ -1,8 +1,9 @@
-select* from `hst-953-2018.team_h.pt_receive_ns_under24hrs` 
-where icustay_id not in (
-select icustay_id from `hst-953-2018.team_h.pt_reveice_excluded_fluid` 
+select* from `hst-953-2018.team_h.pt_receive_ns_within24_18_80` 
+where hadm_id not in (
+select hadm_id from `hst-953-2018.team_h.pt_reveice_excluded_fluid` 
 union all 
-select icustay_id from `hst-953-2018.team_h.excluded_icd9_pt_id` 
-where icustay_id is not null
-group by icustay_id
-) and subject_id in (select subject_id from `hst-953-2018.team_h.ptid_under_80` )
+select hadm_id from `hst-953-2018.team_h.excluded_icd9_pt_id` 
+where hadm_id is not null
+group by hadm_id
+
+) 
