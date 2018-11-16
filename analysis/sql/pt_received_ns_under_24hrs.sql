25c8bb40f832503a26510464075f905acb0cc166
MIngyulu <q8888620002@users.noreply.github.com>
2018-11-16 11:03:55 -0500

    Update pt_received_ns_under_24hrs.sql

--- analysis/sql/pt_received_ns_under_24hrs.sql
+++ analysis/sql/pt_received_ns_under_24hrs.sql
@@ -1,11 +1,11 @@
-with cv as( select icustay_id, subject_id, sum(amount) as total_ns_cv
-from `hst-953-2018.team_h.cv_ns` 
-where hr <24 and amount >= 100
-group by icustay_id, subject_id),
- mv as(select icustay_id, subject_id, sum(amount) as total_ns_mv
-from `hst-953-2018.team_h.mv_ns` 
-where hr <24 and amount >= 100
-group by icustay_id, subject_id)
+with cv as( select icustay_id, subject_id, hadm_id, sum(amount) as total_ns_cv, min(starttime) as ns_given_time
+from `hst-953-2018.team_h.ns_cv_v1` 
+where hr <= 24 and amount >= 100
+group by icustay_id, subject_id,hadm_id),
+ mv as(select icustay_id, subject_id, hadm_id, sum(amount) as total_ns_mv, min(starttime) as ns_given_time
+from `hst-953-2018.team_h.ns_mv_v1` 
+where hr <=24 and amount >= 100
+group by icustay_id, subject_id,hadm_id)
 select * from cv
 union all 
 select * from mv 
