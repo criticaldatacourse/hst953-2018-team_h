cc359e7927e5a007b67227db8677a6dd3264321a
MIngyulu <q8888620002@users.noreply.github.com>
2018-11-16 20:57:46 -0500

    Create pt_within_inclusion_icd9.sql

---
+++ analysis/pt_within_inclusion_icd9.sql
@@ -0,0 +1,6 @@
+select * from `hst-953-2018.team_h.pt_receive_ns_within24_18_80` 
+where hadm_id not in (
+select hadm_id from `hst-953-2018.team_h.excluded_icd9_pts` 
+where hadm_id is not null
+group by hadm_id
+)
