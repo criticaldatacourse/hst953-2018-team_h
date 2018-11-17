b9e27b39a5a787c3f969224224a941b6aa30dc20
MIngyulu <q8888620002@users.noreply.github.com>
2018-11-16 21:04:02 -0500

    Update pt_included_received_ns.sql

--- analysis/sql/pt_included_received_ns.sql
+++ analysis/sql/pt_included_received_ns.sql
@@ -1,9 +1,8 @@
-select* from `hst-953-2018.team_h.pt_receive_ns_within24_18_80` 
+select * from `hst-953-2018.team_h.pt_receive_ns_within24_18_80` 
 where hadm_id not in (
-select hadm_id from `hst-953-2018.team_h.pt_reveice_excluded_fluid` 
+select hadm_id from `hst-953-2018.team_h.excluded_icd9_pts` 
 union all 
-select hadm_id from `hst-953-2018.team_h.excluded_icd9_pt_id` 
+select hadm_id from `hst-953-2018.team_h.pt_reveice_excluded_fluid`
 where hadm_id is not null
 group by hadm_id
-
-) 
+)
