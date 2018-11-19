cdcdae27d8ba5a30a68f0149f70beaf6d6f10e27
MIngyulu <q8888620002@users.noreply.github.com>
2018-11-19 00:58:55 -0500

    Update hgb_fluid.sql

--- analysis/sql/hgb_fluid.sql
+++ analysis/sql/hgb_fluid.sql
@@ -1,6 +1,9 @@
-select hgb.subject_id,  hgb.icustay_id, hgb.hadm_id, hgb.HEMOGLOBIN_1st, 
+
+select hgb.subject_id,  hgb.icustay_id, hgb.hadm_id, hgb.ICU_admitted, hgb.first_measurement, 
+hgb.last_measurement,
+hgb.HEMOGLOBIN_1st, 
 hgb.hemoglobin_24, hgb.hgb_difference, fluid.total_ns_cv
-from `hst-953-2018.team_h.hgb_differ_v1` as hgb
+from `hst-953-2018.team_h.hgb_differ` as hgb
 left join `hst-953-2018.team_h.valid_pt_received_ns` as fluid
 on hgb.icustay_id = fluid.icustay_id
-where total_ns_cv is not null
+where total_ns_cv is not null 
