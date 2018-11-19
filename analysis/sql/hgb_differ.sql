3cc169b90029cedca39877a25d1dc8cbb53e150a
MIngyulu <q8888620002@users.noreply.github.com>
2018-11-19 01:27:24 -0500

    Update hgb_differ.sql

--- analysis/sql/hgb_differ.sql
+++ analysis/sql/hgb_differ.sql
@@ -4,6 +4,6 @@ h.HEMOGLOBIN_24,
 round(  h.HEMOGLOBIN_24- n.HEMOGLOBIN_1st ,4) as hgb_difference
 from `hst-953-2018.team_h.hgb_1` as n
 left join `hst-953-2018.team_h.hgb_24` as h
-on n.icustay_id = h.icustay_id
-where n.HEMOGLOBIN_1st is not null and h.HEMOGLOBIN_24 is not null
+on n.icustay_id = h.icustay_id and h.hadm_id = n.hadm_id 
+where n.HEMOGLOBIN_1st is not null and h.HEMOGLOBIN_24 is not null and first_measurement != last_measurement
 order by subject_id 
