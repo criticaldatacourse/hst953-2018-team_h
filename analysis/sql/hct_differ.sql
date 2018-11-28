998091a1644ce3679e5d4d5ec5fbfe3f7443edb0
MIngyulu <q8888620002@users.noreply.github.com>
2018-11-28 10:52:57 -0500

    Update hct_differ.sql

--- analysis/sql/hct_differ.sql
+++ analysis/sql/hct_differ.sql
@@ -1,7 +1,7 @@
 select n.subject_id, n.hadm_id, n.icustay_id,h.admission_time as ICU_admitted, n.first_measurement, h.last_measurement,
 n.Hct_1st, 
 h.Hct_24,
-round(  h.Hct_24- n.Hct_1st ,4) as hco3_difference
+round(  h.Hct_24- n.Hct_1st ,4) as hct_difference
 from `hst-953-2018.team_h.hct_1st` as n
 left join `hst-953-2018.team_h.hct_24` as h
 on n.icustay_id = h.icustay_id and h.hadm_id = n.hadm_id 
